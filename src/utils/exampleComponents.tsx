5a7e0ccd7aea81f643e91dd19567a59ae4807f73
Tim Rybicki <t.rybicki@gmail.com>
2020-05-15 11:00:58 -0400

    Remove classnames

--- src/utils/exampleComponents.tsx
+++ src/utils/exampleComponents.tsx
@@ -7,7 +7,7 @@ export const CustomInfoBox = (props: any) => (
   <Popper placement="right">
     {({ ref, style, placement, arrowProps }) => (
       <Info ref={ref} data-placement={placement} style={style}>
-        <Arrow className="arrow" ref={arrowProps.ref} style={arrowProps.style} />
+        <Arrow ref={arrowProps.ref} style={arrowProps.style} />
         <Title style={{ fontSize: '16px' }}>Custom Info</Title>
         <span>p: {props.computedStyle.padding}</span>
       </Info>
